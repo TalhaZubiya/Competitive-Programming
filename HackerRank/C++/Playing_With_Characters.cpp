a5de1a43c1412202b6da6f9c529e438f05836126
TALHA ZUBIYA <talhazubiya@gmail.com>
2026-09-07 04:28:25 +0600

    Add Playing With Characters solution

---
+++ HackerRank/C++/Playing_With_Characters.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a;
+    long long b;
+    char c;
+    float d;
+    double e;
+
+    cin >> a >> b >> c >> d >> e;
+
+    printf("%d\n", a);
+    printf("%lld\n", b);
+    printf("%c\n", c);
+    printf("%.3f\n", d);
+    printf("%.9lf\n", e);
+
+    return 0;
+}
