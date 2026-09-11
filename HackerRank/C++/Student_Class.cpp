0d8e0e5f7008d8118d8baea75ae52bfe7589010b
TALHA ZUBIYA <talhazubiya@gmail.com>
2026-09-11 23:40:53 +0600

    Add Student Class solution

---
+++ HackerRank/C++/Student_Class.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age, standard;
+    string first_name, last_name;
+
+    cin >> age;
+    cin >> first_name;
+    cin >> last_name;
+    cin >> standard;
+
+    cout << age << " " << first_name << " " << last_name << " " << standard;
+
+    return 0;
+}
