695c1e279b8b54831ef37023a50a2461c4bd7708
Talha Zubiya <talhazubiya@gmail.com>
2026-09-16 16:37:18 +0600

    Add 5 more Beecrowd C solutions

---
+++ Beecrowd/C/1012.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    double A, B, C, TRIANGULO, CIRCULO, TRAPEZIO, QUADRADO, RETANGULO;
+    scanf("%lf %lf %lf", &A, &B, &C);
+    TRIANGULO = 0.5*A*C;
+    printf("TRIANGULO: %.3lf\n", TRIANGULO);
+    CIRCULO = 3.14159*C*C;
+    printf("CIRCULO: %.3lf\n", CIRCULO);
+    TRAPEZIO = 0.5*(A+B)*C;
+    printf("TRAPEZIO: %.3lf\n", TRAPEZIO);
+    QUADRADO = B*B;
+    printf("QUADRADO: %.3lf\n", QUADRADO);
+    RETANGULO = A*B;
+    printf("RETANGULO: %.3lf\n", RETANGULO);
+ 
+    return 0;
+}
